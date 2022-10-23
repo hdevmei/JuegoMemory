2ad0140602374e8c22b7276bb3e3e1d51e2233d3
hdevmei <hdevmei@gmail.com>
2022-10-23 20:26:25 +0200

    Initial Commit

---
+++ JuegoMemory/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  JuegoMemory
+//
+//  Created by mei_yocontrolo on 22/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+}
+
---
+++ mostrarImagenesViewController.swift
@@ -0,0 +1,23 @@
+//
+//  mostrarImagenesViewController.swift
+//  JuegoMemory
+//
+//  Created by mei_yocontrolo on 23/10/2022.
+//
+
+import UIKit
+
+class mostrarImagenesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBOutlet weak var imagenMostrada: UIImageView!
+    
+    
+ 
+    
+
+}
